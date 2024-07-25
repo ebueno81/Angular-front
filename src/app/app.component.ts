982dc904015eebcbb5b5a2dee477517bdd33934e
EBUENO81 <ebueno81@hotmail.com>
2024-07-25 10:04:19 -0500

    10:04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,9 @@ import { RouterOutlet, Routes } from '@angular/router';
 import { HomeComponent } from "./component/home/home.component";
 import { ProductListComponent } from "./component/product-list/product-list.component";
 import { ProductAddComponent } from "./component/product-add/product-add.component";
-<<<<<<< HEAD
-import { CategoryListComponent } from './component/category-list/category-list.component';
-=======
 import { provideToastr } from 'ngx-toastr';
->>>>>>> 488c41bfa4ddd1772dfdf942037e324fd9b23c02
+import { CategoriasListComponent } from './component/category/categorias-list/categorias-list.component';
+import { CategoriasComponent } from './component/category/categorias/categorias.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent}
@@ -16,7 +14,7 @@ const routes: Routes = [
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, ProductListComponent, ProductAddComponent, CategoryListComponent],
+  imports: [RouterOutlet, HomeComponent, ProductListComponent, ProductAddComponent, CategoriasListComponent, CategoriasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/component/category/categorias-list/categorias-list.component.ts
+++ src/app/component/category/categorias-list/categorias-list.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { HeaderAdminComponent } from "../../header-admin/header-admin.component";
+import { Category } from '../../../common/category';
+import { ToastrService } from 'ngx-toastr';
+import { CategoryService } from '../../../services/category.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-categorias-list',
   standalone: true,
-  imports: [HeaderAdminComponent],
+  imports: [CommonModule, HeaderAdminComponent],// el CommonModule permission work with ngFor
   templateUrl: './categorias-list.component.html',
   styleUrl: './categorias-list.component.css'
 })
 export class CategoriasListComponent {
+
+  categories : Category[]=[];
+  
+  ngOnit(): void{
+    throw new Error('Method not implemented');
+  }
+
+  
+  constructor(private categoryService:CategoryService, private toastr:ToastrService){
+    this.listaCategories();
+  }
+
+  listaCategories(){
+    this.categoryService.getListCategory().subscribe(
+      data=>this.categories=data
+    )
+  }
 
 }
